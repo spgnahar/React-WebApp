bb1cce484143d610cdc35826487a43c96e3bb9bb
shreypra <shreypra@usc.edu>
2020-05-13 03:41:55 -0700

    HW6 Added

---
+++ Assignment 6/README.md
@@ -0,0 +1,30 @@
+# CSCI 571
+
+### The third homework in the CSCI Web Technologies course taught at USC
+
+## Technologies Used:
+
+# To Develop the Front-End 
+HTML, CSS, JavaScript, JSON
+
+# To Develop the Back-End
+FLASK(Python)
+
+# Deployment
+AWS
+
+
+[Full description of the assignment](https://github.com/spgnahar/CSCI-571-Web-Technologies/blob/master/Assignment%206/HW6_Description.pdf).
+## Screenshots of the Working 
+
+![Imgur](https://i.imgur.com/fqOKKJ1.png)
+
+![Imgur](https://i.imgur.com/u0dtoyk.png)
+
+![Imgur](https://i.imgur.com/xiBkzCJ.png)
+
+[Imgur](https://i.imgur.com/3kHAXdI.png)
+
+[Imgur](https://i.imgur.com/YbDmnm1.png)
+
+[Imgur](https://i.imgur.com/gjX4VRm.png)
---
+++ Assignment 6/application.py
@@ -0,0 +1,162 @@
+from newsapi import NewsApiClient
+from flask import Flask, jsonify,render_template,request
+import json
+import re
+
+application = Flask(__name__)
+application.config["DEBUG"] = True
+
+
+@application.route('/')
+def homepage():
+    return application.send_static_file("false.html")
+
+
+
+@application.route('/api',methods=['GET','POST'])
+def api_m():
+    a={}
+    word=[]
+    words={}
+    abc=[]
+    abc1=[]
+    abc2=[]
+    j=0
+    newsapi = NewsApiClient(api_key='8e35f48c905e43e7a7835ebde2de2a83')
+
+    stop=set(line.strip() for line in open('stopwords_en.txt'))
+    top_headlines_general = newsapi.get_top_headlines(language='en',page_size=30)
+    i=0
+    for ele in top_headlines_general["articles"]:
+        w=ele["title"].split()
+        for we in w:
+            we=we.lower()
+            if we not in stop:
+                words[we] = words.get(we, 0) + 1 
+    for ele in top_headlines_general["articles"]:
+        if((ele["source"]!=None) and (ele["author"]!=None) and (ele["title"]!=None) and (ele["description"]!=None) and (ele["url"]!=None) and (ele["urlToImage"]!=None) and (ele["publishedAt"]!=None) and (ele["content"]!=None)):
+            i+=1
+            abc.append(ele)
+            if(i==5):
+                break
+    top_headlines_general["articles"]=abc
+    a['general']=top_headlines_general
+    p=sorted(words.items(), key=lambda x: x[1], reverse=True)
+    
+    for elem in p:
+        j+=1
+        word.append(elem[0])
+        if(j==30):
+            break
+    a["word"]=word
+
+    top_headlines_cnn = newsapi.get_top_headlines(sources='cnn',language='en', page_size=30)
+    i=0
+    for ele in top_headlines_cnn["articles"]:
+        if((ele["source"]!=None) and (ele["source"]["id"]!=None) and (ele["source"]["name"]!=None) and (ele["author"]!=None) and (ele["title"]!=None) and (ele["description"]!=None) and (ele["url"]!=None) and (ele["urlToImage"]!=None) and (ele["publishedAt"]!=None) and (ele["content"]!=None)):
+            i+=1
+            abc1.append(ele)
+            if(i==4):
+                break
+    top_headlines_cnn["articles"]=abc1
+    a['cnn']=top_headlines_cnn
+
+    top_headlines_fox = newsapi.get_top_headlines(sources='fox-news',language='en', page_size=30)
+    i=0
+    for ele in top_headlines_fox["articles"]:
+        if((ele["source"]!=None) and (ele["author"]!=None) and (ele["title"]!=None) and (ele["description"]!=None) and (ele["url"]!=None) and (ele["urlToImage"]!=None) and (ele["publishedAt"]!=None) and (ele["content"]!=None)):
+            i+=1
+            abc2.append(ele)
+            if(i==4):
+                break
+    top_headlines_fox["articles"]=abc2
+    a['fox']=top_headlines_fox
+    return a
+
+@application.route('/formed',methods=['GET','POST'])
+def form():
+    abc3=[]
+    
+    newsapi = NewsApiClient(api_key='8e35f48c905e43e7a7835ebde2de2a83')
+    try:
+
+        if(request.args.get('category')=='all' and request.args.get('sour')=='all'):
+            all_articles = newsapi.get_everything(q=request.args.get('keyword'),
+                                          # sources=request.args.get('source'),
+                                          # domains=request.args.get('category'),
+                                          from_param=request.args.get('from'),
+                                          to=request.args.get('to'),
+                                          language='en',
+                                          sort_by='publishedAt',
+                                          page_size=30)
+        elif(request.args.get('category')=='all' and request.args.get('sour')!='all'):
+            all_articles = newsapi.get_everything(q=request.args.get('keyword'),
+                                          sources=request.args.get('sour'),
+                                          # domains=request.args.get('category'),
+                                          from_param=request.args.get('from'),
+                                          to=request.args.get('to'),
+                                          language='en',
+                                          sort_by='publishedAt',
+                                          page_size=30)
+        elif(request.args.get('category')!='all' and request.args.get('sour')=='all'):
+            cate=request.args.get('category')
+            out=""
+            text = newsapi.get_sources(category=cate,language='en',country='us')
+            for ele in text['sources']:
+                out+=ele['id']+","
+            all_articles = newsapi.get_everything(q=request.args.get('keyword'),sources=out[:len(out)],
+                                          #sources=request.args.get('source'),
+                                          # domains=request.args.get('category'),
+                                          from_param=request.args.get('from'),
+                                          to=request.args.get('to'),
+                                          language='en',
+                                          sort_by='publishedAt',
+                                          page_size=30)
+        else:
+            all_articles = newsapi.get_everything(q=request.args.get('keyword'),#sources=out[:len(out)],
+                                          sources=request.args.get('sour'),
+                                          # domains=request.args.get('category'),
+                                          from_param=request.args.get('from'),
+                                          to=request.args.get('to'),
+                                          language='en',
+                                          sort_by='publishedAt',
+                                          page_size=30)
+
+        for ele in all_articles["articles"]:
+            if((ele["source"]!=None) and (ele["source"]["name"]!=None) and (ele["author"]!=None) and (ele["title"]!=None) and (ele["description"]!=None) and (ele["url"]!=None) and (ele["urlToImage"]!=None) and (ele["publishedAt"]!=None) and (ele["content"]!=None)):
+                abc3.append(ele)
+        all_articles["articles"]=abc3
+       
+        file1=open("output.txt","w")
+        file1.write(json.dumps(all_articles))
+
+        return all_articles
+
+    except Exception as inst:
+        msg=inst['message']
+        abc3['message']=msg
+    return abc3
+
+
+@application.route('/catego',methods=['GET','POST'])
+def inp():
+    newsapi = NewsApiClient(api_key='8e35f48c905e43e7a7835ebde2de2a83')
+    b={}
+    text=request.args.get('category')
+    if(text=='all'):
+        source = newsapi.get_sources()
+    else:
+        source = newsapi.get_sources(category=text.lower(),language='en',country='us')
+    out=list()
+    for ele in source['sources']:
+        out.append(ele['id'])
+    b['keys']=out
+    return b
+
+
+    
+
+
+if __name__ == '__main__':
+    application.run(debug=True)
+    
